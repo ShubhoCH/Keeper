7895595958d5308a3566d836bb6da39396fb3c0a
SHUBHO CHAKRABORTY <shubho1chakraborty1@gmail.com>
2021-10-21 13:56:47 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+
+
+//////////////WITHOUT SIMPLIFICATION:
+// function createNotes(noteItem){
+//     return <Note
+//         key = {noteItem.key}
+//         title = {noteItem.title}
+//         content = {noteItem.content}
+//     />
+// }
+
+// function App(){
+//     return <div>
+//         <Header />
+//         {notes.map(createNotes)};
+//         <Footer />
+//     </div>
+// }
+
+/////////////WITH SIMPLIFICATION:
+
+function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
